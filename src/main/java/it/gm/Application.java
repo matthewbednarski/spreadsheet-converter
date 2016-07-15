dafd02ba6de37c07973df4f31c50a3912e4fc456
Matthew Bednarski <matthew.bednarski@ekr.it>
2016-07-15 15:53:23 +0200

    initial demo implementation

---
+++ src/main/java/it/gm/Application.java
@@ -0,0 +1,98 @@
+package it.gm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by matthew on 14.07.16.
+ */
+public class Application {
+
+    static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        if (args.length == 2) {
+            Path input = Paths.get(args[0]);
+            Path output = Paths.get(args[1]);
+            if (Files.exists(input)) {
+                String fileName = getFileNameWithoutExtension(input);
+                if (getFileNameWithoutExtension(output).equals(getFileNameWithoutExtension(input))
+                        || output.getFileName().endsWith("csv")
+                        ) {
+                    // is filePath
+                    fileName = getFileNameWithoutExtension(output);
+                    output = output.getParent();
+                }
+                if (!Files.exists(output)) {
+                    try {
+                        Path created = Files.createDirectories(output);
+                    } catch (IOException e) {
+                        logger.error(e.getLocalizedMessage());
+                        if (logger.isDebugEnabled()) {
+                            logger.error(e.getLocalizedMessage(), e);
+                        }
+                    }
+                }
+                Spreadsheet s = new Spreadsheet();
+                Map<String, String> out = s.convertToCsv(input, '\t');
+                // if wb has only one sheet
+                if (out.keySet().size() == 1) {
+                    String old_key = out.keySet().iterator().next();
+                    String value = out.get(old_key);
+                    out.remove(old_key);
+                    out.put(fileName + ".csv", value);
+                } else {
+                    //loop through sheets
+                    List<String> sheets = new ArrayList<>(out.keySet());
+                    for (String name : sheets) {
+                        String nextName = fileName + "_" + name + ".csv";
+                        out.put(nextName, out.get(name));
+                        out.remove(name);
+                    }
+                }
+                writeFileMap(out, output);
+            }
+        }
+    }
+
+    protected static void writeFileMap(Map<String, String> map, Path dir) {
+        List<String> sheets = new ArrayList<>(map.keySet());
+        for (String name : sheets) {
+            Path f = Paths.get(dir.toString(), name);
+            logger.info("Writing file "  + name + " to directory: "  + dir.toAbsolutePath().toString());
+            try (FileOutputStream fos = new FileOutputStream(f.toFile())) {
+                fos.write(
+                        map.get(name).getBytes(
+                                Charset.forName("UTF-8")
+                        )
+                );
+                fos.flush();
+            } catch (FileNotFoundException e) {
+                logger.error(e.getLocalizedMessage(), e);
+            } catch (IOException e) {
+                logger.error(e.getLocalizedMessage(), e);
+            }
+
+        }
+    }
+
+    protected static String getFileNameWithoutExtension(Path path) {
+        String fileName = path.getFileName().toString();
+        int pos = fileName.lastIndexOf(".");
+        if (pos > 0) {
+            fileName = fileName.substring(0, pos);
+        }
+        return fileName;
+    }
+}
---
+++ src/main/java/it/gm/Spreadsheet.java
@@ -0,0 +1,110 @@
+package it.gm;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created by matthew on 14.07.16.
+ */
+public class Spreadsheet {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public Map<String, String> convertToCsv(Path spreadsheet, char separator){
+        if(Files.exists(spreadsheet)) {
+            Workbook wb = null;
+            try (InputStream is = new FileInputStream(spreadsheet.toFile())){
+                wb = WorkbookFactory.create(is);
+                return this.convertToCsv(wb, separator);
+            } catch (InvalidFormatException e) {
+                logger.warn(e.getLocalizedMessage());
+                if (logger.isDebugEnabled()) {
+                    logger.debug(e.getLocalizedMessage(), e);
+                }
+            } catch (FileNotFoundException e) {
+                logger.warn(e.getLocalizedMessage());
+                if (logger.isDebugEnabled()) {
+                    logger.debug(e.getLocalizedMessage(), e);
+                }
+            } catch (IOException e) {
+                logger.warn(e.getLocalizedMessage());
+                if (logger.isDebugEnabled()) {
+                    logger.debug(e.getLocalizedMessage(), e);
+                }
+            }
+        }
+        return null;
+    }
+    protected Map<String, String> convertToCsv(Workbook workbook, char separator) {
+        Map<String, String> result = new HashMap<>();
+        try {
+            if (workbook != null && workbook.getNumberOfSheets() > 0) {
+                for (int indexSheet = 0; indexSheet < workbook.getNumberOfSheets(); indexSheet++) {
+                    Sheet sheet = workbook.getSheetAt(indexSheet);
+                    StringBuffer data = new StringBuffer();
+                    if (sheet != null) {
+                        String name = sheet.getSheetName() + ".csv";
+                        Row row;
+                        Cell cell;
+                        // Iterate through each rows from first sheet
+                        Iterator<Row> rowIterator = sheet.iterator();
+                        while (rowIterator.hasNext()) {
+                            row = rowIterator.next();
+                            for (int cellCount = 0; cellCount < row.getLastCellNum(); cellCount++) {
+                                cell = row.getCell(cellCount, Row.CREATE_NULL_AS_BLANK);
+                                switch (cell.getCellType()) {
+                                    case Cell.CELL_TYPE_ERROR:
+                                        data.append(cell.getErrorCellValue());
+                                        data.append(separator);
+                                        break;
+                                    case Cell.CELL_TYPE_FORMULA:
+                                        data.append(cell.getCachedFormulaResultType());
+                                        data.append(separator);
+                                        break;
+                                    case Cell.CELL_TYPE_BOOLEAN:
+                                        data.append(cell.getBooleanCellValue());
+                                        data.append(separator);
+                                        break;
+                                    case Cell.CELL_TYPE_NUMERIC:
+                                        data.append(cell.getNumericCellValue());
+                                        data.append(separator);
+                                        break;
+                                    case Cell.CELL_TYPE_STRING:
+                                        data.append(cell.getStringCellValue());
+                                        data.append(separator);
+                                        break;
+                                    case Cell.CELL_TYPE_BLANK:
+                                        data.append("");
+                                        data.append(separator);
+                                        break;
+                                    default:
+                                        data.append(cell);
+                                        data.append(separator);
+                                }
+                            }
+                            data.append(System.lineSeparator());
+                        }
+                        result.put(name, data.toString());
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.warn(e.getLocalizedMessage());
+            if (logger.isDebugEnabled()) {
+                logger.debug(e.getLocalizedMessage(), e);
+            }
+        }
+        return result;
+    }
+}
